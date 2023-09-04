a9592fe7d918193e1e87362c1a9fb638a78c78c7
“cCody34git config --global user.name cCody34git config -- global user.name cCody34 <charlotte.cody23@gmail.com>
2023-09-04 16:00:39 +0100

    use getArticleById for ArticleCard and add links to SinglePages

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,5 +1,41 @@
-const ArticleCard = ({ article }) => {
-  const { title, topic, article_img_url, votes, comment_count } = article;
+import { getArticleById } from "../api";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+const ArticleCard = ({ article_id }) => {
+  const [article, setArticle] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    getArticleById(article_id)
+      .then(({ data }) => {
+        setArticle(data);
+        console.log(data);
+        setIsLoading(false);
+        setIsError(false);
+      })
+      .catch(({ message }) => {
+        setIsError(message);
+      });
+  }, []);
+
+  const {
+    article_img_url,
+    comment_count,
+    title,
+    topic,
+    votes,
+  } = article;
+
+  if (isError) {
+    return <p>Error: {isError}</p>;
+  }
+
+  if (isLoading) {
+    return <p>Loading ...</p>;
+  }
 
   return (
     <section className="article-card">
@@ -8,7 +44,9 @@ const ArticleCard = ({ article }) => {
         <p>{votes} votes</p>
         <button>⬇</button>
       </section>
-      <h3 className="article-card-title">{title}</h3>
+      <h3 className="article-card-title">
+        <Link to={`/articles/${article_id}`}>{title}</Link>
+      </h3>
       <img className="article-card-img" src={`${article_img_url}`}></img>
       <section className="article-card-info">
         <p className="article-card-comment-count">Comments: {comment_count}</p>
