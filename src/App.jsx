c28abbdb4d00c183b75fa68887b806e725986d04
“cCody34git config --global user.name cCody34git config -- global user.name cCody34 <charlotte.cody23@gmail.com>
2023-09-04 11:04:38 +0100

    create basic navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
       <Header />
+      <NavBar />
     </>
   );
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,5 @@
+const NavBar = () => {
+  return <h2>NavBar here</h2>;
+};
+
+export default NavBar;
