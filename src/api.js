343635501017dcaa6abab7fdc7e719a278b7be69
“cCody34git config --global user.name cCody34git config -- global user.name cCody34 <charlotte.cody23@gmail.com>
2023-09-04 14:49:27 +0100

    use axios.create

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const api = "https://news-ynvj.onrender.com/api";
+const newsApi = axios.create({ baseURL: "https://news-ynvj.onrender.com/api" });
 
 export const getArticles = () => {
-  return axios.get(`${api}/articles`);
+  return newsApi.get("/articles");
 };
 
 export const getArticleById = (article_id) => {
-  return axios.get(`${api}/articles/${article_id}`);
+  return newsApi.get(`/articles/${article_id}`);
 };
