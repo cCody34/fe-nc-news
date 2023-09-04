e3fad554215577dbe0b402c396a837469576a0cf
“cCody34git config --global user.name cCody34git config -- global user.name cCody34 <charlotte.cody23@gmail.com>
2023-09-04 11:09:54 +0100

    setup initial home route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
+import Home from "./components/pages/Home";
 
 function App() {
   return (
     <>
       <Header />
       <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
     </>
   );
 }
