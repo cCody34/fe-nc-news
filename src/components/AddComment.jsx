13ca0528697a5670585541b1c0b96b76011ded17
“cCody34git config --global user.name cCody34git config -- global user.name cCody34 <charlotte.cody23@gmail.com>
2023-09-05 10:50:14 +0100

    create comment form basic elements

--- src/components/AddComment.jsx
+++ src/components/AddComment.jsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
+
 const AddComment = () => {
-    return (<form>
-        <p>Comment Adder here</p>
-    </form>)
+  const [newComment, setNewComment] = useState("");
+  const handleCommentSubmit = (event) => {
+    event.preventDefault();
+    if (newComment) {
+        console.log("new Comment")
+    }
+    console.log("no comment");
+  };
+  console.log(newComment);
+  return (
+    <form>
+      <input
+        value={newComment}
+        onChange={(event) => {
+          setNewComment(event.target.value);
+        }}
+      ></input>
+      <button onClick={handleCommentSubmit}>Submit</button>
+    </form>
+  );
 };
 
 export default AddComment;
