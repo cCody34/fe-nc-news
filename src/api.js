8f43548b4e8ef62aab3d7835357695b85c0e4905
“cCody34git config --global user.name cCody34git config -- global user.name cCody34 <charlotte.cody23@gmail.com>
2023-09-05 19:12:22 +0100

    edit getArticles using axios params

--- src/api.js
+++ src/api.js
@@ -3,35 +3,9 @@ import axios from "axios";
 const newsApi = axios.create({ baseURL: "https://news-ynvj.onrender.com/api" });
 
 export const getArticles = (topicQuery, sortByQuery, orderQuery) => {
-  const queries = [];
-  if (topicQuery) {
-    queries.push({ topic: topicQuery });
-  }
-  if (sortByQuery) {
-    queries.push({ sort_by: sortByQuery });
-  }
-  if (orderQuery) {
-    queries.push({ order: orderQuery });
-  }
-  let baseApiArticleString = "/articles";
-  if (queries.length === 3) {
-    baseApiArticleString += `?topic=${topicQuery}&sort_by=${sortByQuery}&order=${orderQuery}`;
-  } else if (queries.length === 2) {
-    const firstKey = Object.keys(queries[0])[0];
-    const secondKey = Object.keys(queries[1])[0];
-    baseApiArticleString += `?${firstKey}=${queries[0][firstKey]}&${secondKey}=${queries[1][secondKey]}`;
-  } else if (queries.length === 1) {
-    if (topicQuery) {
-      baseApiArticleString += `?topic=${topicQuery}`;
-    }
-    if (sortByQuery) {
-      baseApiArticleString += `?sort_by=${sortByQuery}`;
-    }
-    if (orderQuery) {
-      baseApiArticleString += `?order=${orderQuery}`;
-    }
-  }
-  return newsApi.get(baseApiArticleString);
+  return newsApi.get("/articles", {
+    params: { topic: topicQuery, sort_by: sortByQuery, order: orderQuery },
+  });
 };
 
 export const getArticleById = (article_id) => {
