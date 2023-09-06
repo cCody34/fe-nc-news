0f20493f4882a163c1cd9e4b04226469c9d325ec
“cCody34git config --global user.name cCody34git config -- global user.name cCody34 <charlotte.cody23@gmail.com>
2023-09-06 09:05:50 +0100

    populate UserCard

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,5 +1,12 @@
-const UserCard = () => {
-  return <p>USER CARD</p>;
+const UserCard = ({ user }) => {
+  console.log(user);
+  return (
+    <section className="user-card">
+      <h3 className="user-card-username">{user.username}</h3>
+      <img className="user-card-avatar" src={user.avatar_url}></img>
+      <p className="user-card-name">{user.name}</p>
+    </section>
+  );
 };
 
 export default UserCard;
