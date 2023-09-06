4d5e175352741718657a4a1c1daecd93d6cb236f
“cCody34git config --global user.name cCody34git config -- global user.name cCody34 <charlotte.cody23@gmail.com>
2023-09-06 09:41:34 +0100

    set up userContext

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
+import { useContext } from "react";
+import { UserContext } from "./contexts/User";
+
 const Header = () => {
+  const {user} = useContext(UserContext);
   return (
     <section className="header">
       <h1>Header here</h1>
+      <p>Logged in as: {user.name}</p>
     </section>
   );
 };
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,6 +1,17 @@
+import { useContext } from "react";
+import { UserContext } from "./contexts/User";
+
 const UserCard = ({ user }) => {
+  const { setUser } = useContext(UserContext);
+
   return (
-    <button className="user-card">
+    <button
+      className="user-card"
+      onClick={(event) => {
+        event.preventDefault();
+        setUser(user);
+      }}
+    >
       <h3 className="user-card-username">{user.username}</h3>
       <img className="user-card-avatar" src={user.avatar_url}></img>
       <p className="user-card-name">{user.name}</p>
