d523074603bd51b859fd0af90d0ce9a5aa9f4759
“cCody34git config --global user.name cCody34git config -- global user.name cCody34 <charlotte.cody23@gmail.com>
2023-09-07 15:27:17 +0100

    add active-button class to nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,32 @@
 import { Link } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 
 const NavBar = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const topicQuery = searchParams.get("topic");
+  console.log(topicQuery);
+
   return (
     <section className="nav-bar">
       <h2>Menu</h2>
       <h3>View articles by topic:</h3>
       <Link to="/articles?topic=cooking">
-        <button>Cooking</button>
+        <button className={topicQuery === "cooking" && "active-topic"}>
+          Cooking
+        </button>
       </Link>
       <Link to="/articles?topic=coding">
-        <button>Coding</button>
+        <button className={topicQuery === "coding" && "active-topic"}>
+          Coding
+        </button>
       </Link>
       <Link to="/articles?topic=football">
-        <button>Football</button>
+        <button className={topicQuery === "football" && "active-topic"}>
+          Football
+        </button>
       </Link>
       <Link to="/articles">
-        <button>All Articles</button>
+        <button className={!topicQuery && "active-topic"}>All Articles</button>
       </Link>
     </section>
   );
