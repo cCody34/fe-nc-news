260da6a9bde86e26a0eecc4e0a0a9719007e14c3
“cCody34git config --global user.name cCody34git config -- global user.name cCody34 <charlotte.cody23@gmail.com>
2023-09-07 16:40:48 +0100

    fix home styling

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <>
+    <section className="home">
       <ArticleList limit={5} />
       <Link to="/articles">
         <button>View all articles</button>
       </Link>
-    </>
+    </section>
   );
 };
 
