2f12bdbb9a85d2d8e2fc76de5269421f006da40f
“cCody34git config --global user.name cCody34git config -- global user.name cCody34 <charlotte.cody23@gmail.com>
2023-09-07 16:28:29 +0100

    desktop styling

--- src/components/pages/ErrorPage.jsx
+++ src/components/pages/ErrorPage.jsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
-    <>
+    <section className="error-page">
       <h2>404: PAGE NOT FOUND</h2>
       <label>
         Double check that URL is correct or <Link to="/">click here</Link> to go
         to Home page.
       </label>
-    </>
+    </section>
   );
 };
 
