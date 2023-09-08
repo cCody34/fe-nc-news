572b1bb3b63efc0e4e07cb378e93612c94b0e621
“cCody34git config --global user.name cCody34git config -- global user.name cCody34 <charlotte.cody23@gmail.com>
2023-09-08 14:55:36 +0100

    add SingleUser page

--- src/components/pages/SingleUser.jsx
+++ src/components/pages/SingleUser.jsx
@@ -1,15 +1,38 @@
 import { useEffect, useState } from "react";
 import { getUserByName } from "../../api";
+import { useParams } from "react-router-dom";
+import Error from "../Error";
 
 const SingleUser = () => {
+  const { username } = useParams();
   const [singleUser, setSingleUser] = useState({});
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
-    getUserByName("jessjelly").then(({ data }) => {
-      console.log(data);
-      setSingleUser(data);
-    });
+    getUserByName(username)
+      .then(({ data }) => {
+        setSingleUser(data);
+        setIsError(false);
+      })
+      .catch((err) => {
+        setIsError(err);
+      });
   }, []);
+
+  if (isError) {
+    if (isError.response.data.msg) {
+      return (
+        <Error
+          errCode={isError.response.status}
+          errMsg={isError.response.data.msg}
+        />
+      );
+    } else {
+      return (
+        <Error errCode={isError.response.status} errMsg={isError.message} />
+      );
+    }
+  }
 
   const displayUserInfo = () => {
     return (
